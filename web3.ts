0c5a0b7dc1e0117ec4159bc6165692042d0fef09
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-02-16 17:03:31 +0000

    feat(web3js): adds ERC20 Transfer function
    Tested: Works as expected
    Transaction: https://alfajores.celoscan.io/tx/0x7c3d89e1c89affe40837a85292ac363a7a7d47fe856199f2c8978d314be51826

--- web3.ts
+++ web3.ts
@@ -1,7 +1,46 @@
-import { Web3 } from 'web3';
-import { Connection, CeloProvider } from '@celo/connect';
+import Web3 from "web3";
+import { Connection } from "@celo/connect";
+import { ERC20ABI } from "./erc20Abi";
+import "dotenv/config"; // use to read private key from environment variable
 
-const web3 = new Web3("https://alfajores-forno.celo-testnet.org") // Celo testnet
-const connection = new Connection(web3)
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+if (!PRIVATE_KEY) {
+    throw new Error(
+        "PRIVATE_KEY is not set in .env file. Please set PRIVATE_KEY=<your_private_key> in .env file."
+    );
+}
+const RECIPIENT = "0x22579CA45eE22E2E16dDF72D955D6cf4c767B0eF"; // arbitrary address
+const CONTRACT_ADDRESS = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"; // cUSD contract address
 
-connection.setProvider(CeloProvider)
+// Set up web3js client
+const web3 = new Web3("https://alfajores-forno.celo-testnet.org"); // Celo testnet
+const connection = new Connection(web3);
+
+// Set up account
+const account = web3.eth.accounts.wallet.add(`0x${PRIVATE_KEY}`);
+const sender = web3.eth.accounts.wallet[0].address;
+
+// Set up ERC20 contract
+const contract = new web3.eth.Contract(ERC20ABI, CONTRACT_ADDRESS);
+
+async function erc20Transfer() {
+    console.log(`Initiating fee currency transaction...`);
+
+    // TODO: Adjust the amount to send based on the token's decimals (USDC has 6 decimals)
+    const transactionReceipt = await contract.methods
+        .transfer(RECIPIENT, web3.utils.toWei("0.01", "ether"))
+        .send({
+            from: sender,
+            // gasLimit: // TODO: implement gas estimation
+            maxPriorityFeePerGas: web3.utils.toWei("10", "gwei"),
+            maxFeePerGas: web3.utils.toWei("10", "gwei"),
+            // feeCurrency: // TODO: implement fee currency
+        });
+
+    console.log(transactionReceipt);
+}
+
+// Initiate the transfer
+erc20Transfer().catch((err) => {
+    console.error("An error occurred:", err);
+});
