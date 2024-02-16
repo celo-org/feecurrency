161904bac9f99608c34a81b428a16a6e92454f99
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-02-16 17:04:22 +0000

    chore(viem): adds TODO comments
    - implement gas estimation
    - adjust send amount based on ERC20 decimals

--- viem.ts
+++ viem.ts
@@ -1,10 +1,14 @@
 import { createPublicClient, createWalletClient, http, parseEther, parseGwei } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { celoAlfajores } from "viem/chains";
-
 import "dotenv/config"; // use to read private key from environment variable
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+if (!PRIVATE_KEY) {
+    throw new Error(
+        "PRIVATE_KEY is not set in .env file. Please set PRIVATE_KEY=<your_private_key> in .env file."
+    );
+}
 const RECIPIENT = "0x22579CA45eE22E2E16dDF72D955D6cf4c767B0eF"; // arbitrary address
 
 /**
@@ -25,8 +29,10 @@ async function nativeTransfer() {
     const transactionHash = await walletClient.sendTransaction({
         account, // Sender
         to: RECIPIENT, // recipient
+        // TODO: Adjust the amount to send based on the token's decimals (USDC has 6 decimals)
         value: parseEther("0.01"), // 0.01 CELO
         feeCurrency: "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1", // cUSD fee currency
+        // gas: // TODO: implement gas estimation
         maxFeePerGas: parseGwei("10"), // Special field for dynamic fee transaction type (EIP-1559)
         maxPriorityFeePerGas: parseGwei("10"), // Special field for dynamic fee transaction type (EIP-1559)
     });
@@ -39,6 +45,5 @@ async function nativeTransfer() {
 }
 
 nativeTransfer().catch((err) => {
-    // Handle any errors that might occur in the demos
     console.error("An error occurred:", err);
 });
