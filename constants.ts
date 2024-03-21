7263e14eee82afcdcd9d92228b77a9a0dce4ccc2
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-03-21 17:42:08 -0300

    chore(USDC): fix token and adapter addresses
    Previously mistakenly only changed variable names, but not addresses.

--- constants.ts
+++ constants.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 
 export const RECIPIENT = "0x22579CA45eE22E2E16dDF72D955D6cf4c767B0eF"; // arbitrary address
 export const cUSD_TOKEN_ADDRESS = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"; // ERC20
-export const USDC_TOKEN_ADDRESS = "0x780c1551C2Be3ea3B1f8b1E4CeDc9C3CE40da24E"; // ERC20
-export const USDC_ADAPTER_ADDRESS = "0xDB93874fE111F5a87Acc11Ff09Ee9450Ac6509AE"; // Adapter
+export const USDC_TOKEN_ADDRESS = "0x2F25deB3848C207fc8E0c34035B3Ba7fC157602B"; // ERC20
+export const USDC_ADAPTER_ADDRESS = "0x4822e58de6f5e485eF90df51C41CE01721331dC0"; // Adapter
 export const PRIVATE_KEY = process.env.PRIVATE_KEY;
 if (!PRIVATE_KEY) {
     throw new Error(
